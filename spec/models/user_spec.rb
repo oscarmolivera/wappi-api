07ca44980b83e8930a6b83bb94e3a4ffe1a20484
Oscar M. Olivera <oscarmolivera@gmail.com>
2021-11-03 22:38:20 -0300

    RSpec: Config and test of ShouldMatchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of(:encrypted_password) }
+  end
 end
