34d45dbeb3db265a23f29bfafbd3dfe14d8a75f7
Oscar M. Olivera <oscarmolivera@gmail.com>
2021-11-05 08:16:03 -0300

    MODEL: Adding UserDetails

---
+++ db/migrate/20211104113129_add_details_to_users.rb
@@ -0,0 +1,10 @@
+class AddDetailsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :username, :string
+    add_column :users, :phone, :string
+    add_column :users, :website, :string
+    add_reference :users, :address, null: false, foreign_key: true
+    add_reference :users, :company, null: false, foreign_key: true
+  end
+end
