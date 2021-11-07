5cea96b41c51fb89500ef8c821cf25977b744ebc
Oscar M. Olivera <oscarmolivera@gmail.com>
2021-11-07 17:35:38 -0300

    CONFIG: Schema & Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'users/index'
   root to: 'homes#index'
   devise_for :users
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
